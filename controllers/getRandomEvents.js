c30c8b93d14251b8b65760cfb6bc4d3ca9d49b28
Yousra Bouhriz <yousrabhz1512@gmail.com>
2024-05-29 13:26:23 +0100

    get random events size changes

--- controllers/getRandomEvents.js
+++ controllers/getRandomEvents.js
@@ -3,7 +3,7 @@ const Post = require('../models/post')
 const RandomPosts = async(req , res) => {
     try{
         let  posts = await Post.find()
-         posts = posts.slice(0, 2)
+         posts = posts.slice(0, 20)
         res.json({posts})
 
     }
